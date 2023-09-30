669ba2af55d8a5158bc3fc171426c710840a6b0a
spykelionel <ndilionel6@gmail.com>
2023-09-30 18:12:24 +0100

    format document

--- src/RootRouter.jsx
+++ src/RootRouter.jsx
@@ -19,7 +19,7 @@ const router = createBrowserRouter([
     // localhost:5157/notdefined
   },
   {
-    path: "users", 
+    path: "users",
     element: <Users />,
     children: [
       {
@@ -47,31 +47,31 @@ function PageNotFound() {
 
 function User() {
   const { username } = useParams();
-  const userInfo = useUser(username)
-console.log(userInfo)
+  const userInfo = useUser(username);
+  console.log(userInfo);
   return (
     <div style={{ textAlign: "center" }}>
       <h1>Information about {userInfo.firstName}</h1>
       <p>Age: {userInfo.age}</p>
       <p>Gender: {userInfo.gender}</p>
-      <img src={userInfo.image} alt={userInfo.username} width="70px"/>
+      <img src={userInfo.image} alt={userInfo.username} width="70px" />
       <Link to="/">Go back home</Link>
     </div>
   );
 }
 
-function useUser(username){
-    const users = useOutletContext();
-    const [user, setUser] = useState({})
+function useUser(username) {
+  const users = useOutletContext();
+  const [user, setUser] = useState({});
 
-    useEffect(()=>{
-        const userInfo = users.find(
-            (u) => u.firstName.toLowerCase() === username.toLowerCase()
-          );
-          setUser(userInfo)
-    }, [username])
-   
-        return user
+  useEffect(() => {
+    const userInfo = users.find(
+      (u) => u.firstName.toLowerCase() === username.toLowerCase()
+    );
+    setUser(userInfo);
+  }, [username]);
+
+  return user;
 }
 
 function Contact() {
@@ -88,7 +88,7 @@ function Contact() {
 
 function Users() {
   const [users, setUsers] = useState({ users: [] });
-  const [error, setError] = useState(null)
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     function fetchUsers() {
@@ -97,14 +97,14 @@ function Users() {
         .then((result2) => {
           setUsers(result2);
         })
-        .catch((err)=>{
-            // runs on error
-            console.log(err)
-            setError(err)
+        .catch((err) => {
+          // runs on error
+          console.log(err);
+          setError(err);
         })
-        .finally(()=>{
-            // always runs
-        })
+        .finally(() => {
+          // always runs
+        });
     }
 
     fetchUsers();
